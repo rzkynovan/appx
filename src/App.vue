e4e5e18b12f51e2afb01de3e9446652296560fd0
rzkynovan <rizkynovan21@gmail.com>
2023-03-01 22:13:17 +0700

    belajarapi-FE

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+
+</style>
